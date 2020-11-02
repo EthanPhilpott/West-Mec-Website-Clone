99613df189a900b18c7587639233dd06feb7a5e5
Ethan Philpott <ephilpott2004@gmail.com>
2020-11-01 22:00:21 -0700

    I dont wanna talk about it

--- js/script.js
+++ js/script.js
@@ -1,14 +1,45 @@
-const MAX_STRETCH = 1.5; // defines the amount the cursor can stretch in acordance to the size of the orginal cursor
-const FRICTION = 0.1; // Amount fo friction 
+function AngleDis (dx, dy) {
+    return Math.tan(dy/dx)
+}
 
-let cursor = document.getElementById("cursor");
+class Cursor {
+    constructor (cursorHTML, friction, speed) {
+        this.cursor = cursorHTML;
+        this.friction = friction;
+        this.speed = speed * 1000;
+        this.precision = 2;
 
-document.addEventListener("mousemove", (e) => {
-    var mouseX = e.clientX;
-    var mouseY = e.clientY;
-})
+        this.mouse = {x: 0, y: 0};
+        this.translation = {x: 1, y: 1};
+        
+        this.Events();
+        this.Animate();
+    }
 
-var cursorX = cursor.clientX;
-var cursorY = cursor.clientY;
+    Events () {
+        window.addEventListener ("mousemove", (e) => {
+            this.mouse.x = e.clientX;
+            this.mouse.y = e.clientY;
+        }) 
+    }
 
-function Cursor
+    Animate () {
+        setInterval(() => {
+            this.Render()
+        }, this.speed) 
+    }
+
+    Update () {
+        this.translation.x = (this.mouse.x - this.translation.x) * this.friction;
+        this.translation.y = (this.mouse.y - this.translation.y) * this.friction;
+    }
+
+    Render () {
+        this.Update()
+        this.cursor.style.transform = "translate(" + this.translation.x.toFixed(this.precision) + 'px, ' + this.translation.y.toFixed(this.precision) + 'px)';
+        console.log(this.cursor.style.transform)
+    }
+}
+
+cursor = new Cursor(document.getElementById('cursor'), 0.1, 1)
+
